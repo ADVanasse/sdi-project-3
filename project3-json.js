057fb93d865630d04f2e99f9f712c89db5b7928b
ADVanasse <advanasse@fullsail.edu>
2015-01-22 16:13:41 -0500

    JSON data

---
+++ project3-json.js
@@ -0,0 +1,19 @@
+// JSON Data for Project 3
+
+var json = {
+	"hotels":[
+			{"hotelName" : "Hilton",
+			 "bedType" : "King"
+			 },
+			{
+			"hotelName" : "InterContinental", 
+			"bedType" : "California King"
+			},
+			{
+			"hotelName" : "Marriott",
+			"bedType" : "Twin"
+			}
+		]
+	};
+
+
--- vanasse-alicia-project3.js
+++ vanasse-alicia-project3.js
@@ -14,7 +14,9 @@ var say = function (message) {
 
 
 //Global Variables
-               
+var where = ["here",
+			 "there",
+			 "every and anywhere"];               
 var bigTrip = "Europe";            
 var europe = ["Italy",					
               "Spain",
@@ -23,25 +25,17 @@ var europe = ["Italy",
               "The Netherlands"],
  	 daysEurope = [7, 5, 3, 4, 3];     	
 var travelBug = true;
-<<<<<<< HEAD
 var hotelCost = 150;
 var myVice = "Travel is my vice!";
+var moreTravel = "work less and travel more!";
+
+//Confirm for Boolean Argument
 
 
 
+//Boolean Function
 
-
-//String function
-
-var 
-
-
-=======
-var myMoney = 5000;
-var myVice = "Travel is my vice!";
-var youTravel;
->>>>>>> FETCH_HEAD
-
+	
 // Prompt for Procedure Argument
 
 var letsFly = prompt("Do you like to travel?","Yes or No");
@@ -50,16 +44,15 @@ var letsFly = prompt("Do you like to travel?","Yes or No");
 
     if (letsFly === "Yes") {
         say("Me too, I just got back from a big trip to " + bigTrip + ".");
-         } if (letsFly === "No") {
+         } else if (letsFly === "No") {
                 say("Really? But there's so much around the world to see!");
-<<<<<<< HEAD
-         } else {
-         		say("Huh?");
-    };	
-           
-// Array Function
+    		} else { 
+    				say("That's a weird answer");
+    	};
+    				
+// Array Function with For loop and Nested Loop
 
-var goToCountry = function (whereTo) {
+var goToCountry = function(whereTo) {
     var theCountry = europe[whereTo],
         daysThisCountry = daysEurope[countryNumber];
    			 say( "I was excited to visit " + theCountry +
@@ -67,10 +60,9 @@ var goToCountry = function (whereTo) {
     		for (var days = 0; days < daysThisCountry; days ++) {
         		var daysLeft = daysThisCountry - days;
      				   say("Before I knew it " + days + " had gone by, leaving me with only " +
-            					daysLeft + "to enjoy the country's beauty.");
+            					daysLeft + " to enjoy the country's beauty.");
             			say("I'd had such a great time in " + theCountry + "! As much as I hated leaving, " +
     							"I was really anxious to get to the next country.");
-    							return goToCountry;
     		};
 		};
    				for (var countryNumber = 0; countryNumber < europe.length; countryNumber++) {
@@ -78,39 +70,15 @@ var goToCountry = function (whereTo) {
     					 	
     };
 
-goToCountry();
-=======
-         };
-           
-// Array Function
 
-var goToCountry = function(whereTo) {
-    var theCountry = europe[whereTo],
-        daysThisCountry = daysEurope[countryNumber];
-   			 say( "I was excited to visit " + theCountry +
-        " for " + daysThisCountry + " days. It went by way too fast.");
-    for (var days = 0; days < daysThisCountry; days ++) {
-            var daysLeft = daysThisCountry - days;
-            	say("Before I knew it " + days + " had gone by, leaving me with only " +
-            		daysLeft + "to enjoy the country's beauty.");
-            			return europe;
-        };
-    	say("I'd had such a great time in " + theCountry + "! As much as I hated leaving, " +
-    		"I was really anxious to get to the next country.");
+
+
+// Number Function with  Number argument and Return
+
+var sleepCost = function(number) {
+	return hotelCost * number;
 };
 
-for (var countryNumber = 0; countryNumber < europe.length; countryNumber++) {
-    	goToCountry(countryNumber);
-    	
-};
-
-
-
-
->>>>>>> FETCH_HEAD
-
-
-
-
+say("it cost " + sleepCost(22) + " dollars for the hotels alone. But it was worth every penny!");
 
 
